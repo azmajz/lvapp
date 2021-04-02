4cb0dc28b8ff2b6edf3b4d8a766a16ece6aeedfa
azmajz <53705217+azmajz@users.noreply.github.com>
2021-04-02 08:40:51 +0530

    Update database.php

--- config/database.php
+++ config/database.php
@@ -52,7 +52,7 @@ return [
             'username' => getenv('DB_USERNAME', 'forge'),
             'password' => getenv('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
+            'charset' => 'utf8',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
