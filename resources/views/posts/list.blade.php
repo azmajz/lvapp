28afac24d2939d0f19cdac570ec52695fb4f0c49
azmajz <53705217+azmajz@users.noreply.github.com>
2021-04-02 09:29:31 +0530

    Update list.blade.php

--- resources/views/posts/list.blade.php
+++ resources/views/posts/list.blade.php
@@ -12,7 +12,8 @@
         <div class="card mb-2">
             <div class="card-body p-3">
                 <h5>
-                    <img src="/images/{{$post->cover_image}}" style="width:60px" class="border mr-2">
+                    <img src="data:image/jpeg;base64,{{ base64_encode($post->cover_image) }}" style="width:60px" class="border mr-2" />
+                    {{-- <img src="/images/{{$post->cover_image}}" style="width:60px" class="border mr-2"> --}}
                     <a href="/posts/{{$post->id}}">{{$post->title}}</a>
                     <small class="text-muted float-right">
                         Posted on - {{date('d/M/Y', strtotime($post->created_at))}}
